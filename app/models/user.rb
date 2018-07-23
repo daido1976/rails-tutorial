a434d24b215f84064e96f6a8c76e0cf108ed1df6
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-07-23 19:27:51 +0900

    14.1.4 終了まで！

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token
 
   before_save { email.downcase! }
@@ -37,5 +39,17 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
